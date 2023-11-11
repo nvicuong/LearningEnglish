3574e91254553742f6e2db9421ff5dda493f25e9
hungsinh2k4 <monternsh@gmail.com>
2023-11-11 14:38:12 +0700

    update tiếng việt

--- src/main/java/commandline/DictionaryCommandLine.java
+++ src/main/java/commandline/DictionaryCommandLine.java
@@ -25,21 +25,21 @@ public class DictionaryCommandLine {
         System.out.println("\t[7] Game");
         System.out.println("\t[8] Import from file");
         System.out.println("\t[9] Export to file");
-        System.out.println("Hay chon cac chuc nang ma ban muon [0-9]");
+        System.out.println("Hãy chọn các chức năng mà bạn muốn sử dụng [0-9]");
         Scanner sc = new Scanner(System.in);
         while (true) {
             char s;
             s = sc.next().charAt(0);
             if (s < '0' || s > '9') {
                 System.out.println("Action not supported.");
-                System.out.println("Hay chon cac chuc nang ma ban muon [0-9]");
+                System.out.println("Hãy chọn các chức năng mà bạn muốn sử dụng [0-9]");
             }
             if (s == '0') {
                 BookMarkManager.getBookMarkManager().save();
                 return;
             }
             if (s == '1') {
-                System.out.println("Vui long nhap tu ban muon them vao tu dien");
+                System.out.println("Vui lòng nhập từ mà bạn muốn thêm vào từ điển");
                 String englishWord = sc.next();
                 String vietnameseWord = sc.next();
                 Word word_target = new Word(englishWord, "", vietnameseWord, "");
@@ -47,33 +47,33 @@ public class DictionaryCommandLine {
                 System.out.println(BookMarkManager.getBookMarkManager().getWordList());
             }
             if (s == '2') {
-                System.out.println("Vui long nhap tu ban muon xoa khoi tu dien");
+                System.out.println("Vui lòng nhập từ mà bạn muốn xóa khỏi từ điển");
                 String englishWord = sc.next();
                 Word word_target = new Word(englishWord, "", "", "");
                 BookMarkManager.getBookMarkManager().removeWord(word_target);
                 System.out.println(BookMarkManager.getBookMarkManager().getWordList());
             }
             if (s == '3') {
-                System.out.println("Vui long nhap tu ban muon cap nhat");
+                System.out.println("Vui lòng nhập từ mà bạn muốn thay đổi nó");
             }
             if (s == '4') {
-                System.out.println("Vui long nhap tu ban muon hien thi");
+                System.out.println("Danh sách các từ trong BookMark");
 
             }
             if (s == '5') {
-                System.out.println();
+                System.out.println("Vui lòng nhập từ mà bạn muốn tra cứu");
             }
             if (s == '6') {
-                System.out.println();
+                System.out.println("Vui lòng nhập từ mà bạn muốn tìm kiếm");
             }
             if (s == '7') {
-                System.out.println();
+                System.out.println("Game đoán từ");
             }
             if (s == '8') {
-                System.out.println();
+                System.out.println("Vui lòng nhập đường dẫn file bạn muốn import");
             }
             if (s == '9') {
-                System.out.println();
+                System.out.println("Vui lòng nhập đường dẫn file bạn muốn export");
             }
         }
     }
