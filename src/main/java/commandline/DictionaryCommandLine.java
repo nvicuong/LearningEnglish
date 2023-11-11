1c5391026302ade5b4fe55a06975fa248d5286da
hungsinh2k4 <monternsh@gmail.com>
2023-11-11 22:29:29 +0700

    -Game commandline

--- src/main/java/commandline/DictionaryCommandLine.java
+++ src/main/java/commandline/DictionaryCommandLine.java
@@ -7,10 +7,19 @@ import model.Word;
 import model.WordManager;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class DictionaryCommandLine {
+
+    private static final WordGame word = new WordGame();
+    private static String myWord;
+    private static List<String> myLetters;
+    private static List<String> answer;
+
+    private static int correctWord = 2;
+
+    private static int isFound;
+
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         WordManager.getWordManager();
         BookMarkManager.getBookMarkManager();
@@ -133,15 +142,96 @@ public class DictionaryCommandLine {
             }
             if (s == '6') {
                 System.out.println("Vui lòng nhập từ mà bạn muốn tìm kiếm");
+                String englishWord = sc.next();
+
             }
             if (s == '7') {
-                System.out.println("Game đoán từ");
+                System.out.println("Game Doan Tu : )");
+                myWord = word.getRandomWord();
+                myLetters = Arrays.asList(myWord.split(""));
+//                System.out.println(myLetters);
+                answer = Arrays.asList(new String[myLetters.size()*2]);
+                for(int i = 0; i < myLetters.size() * 2; i++){
+                    if(i % 2 == 0){
+                        answer.set(i, "_");
+                    }else{
+                        answer.set(i, " ");
+                    }
+                }
+                myLetters.set(0,".");
+                myLetters.set(myWord.length() - 1, ".");
+                System.out.println("Từ này có " + myWord.length() + " chữ cái");
+                System.out.println("Từ này bắt đầu bằng chữ " + myWord.charAt(0));
+                System.out.println("Và kết thúc bằng chữ " + myWord.charAt(myWord.length() - 1));
+                answer.set(0, myWord.charAt(0) + "");
+                answer.set(myWord.length() * 2 - 2, myWord.charAt(myWord.length() - 1) + "");
+                for (String c : answer) {
+                    System.out.print(c);
+                }
+                System.out.println();
+                while (true) {
+                    String res;
+                    res = sc.next();
+                    int count = 0;
+                    if (myLetters.contains(res)) {
+                        int letterIndex;
+                        while ((letterIndex = myLetters.indexOf(res)) != -1) {
+                            correctWord++;
+                            myLetters.set(letterIndex, ".");
+                            answer.set(letterIndex * 2, res);
+                            count ++;
+                        }
+                        System.out.println("Có " + count + " chữ " + res + " trong từ");
+                        System.out.println("Như vậy từ của bạn hiện tại là");
+                        for (int i = 0; i < answer.size(); i++) {
+                            System.out.print(answer.get(i));
+                        }
+                        System.out.println();
+                        if (correctWord == myWord.length()) {
+                            System.out.println("Bạn đã thắng");
+                            break;
+                        }
+                    } else {
+                        isFound ++;
+                        System.out.println("Không có chữ " + res + " nào trong từ");
+                        System.out.println("Như vậy từ của bạn hiện tại là");
+                        for (int i = 0; i < answer.size(); i++) {
+                            System.out.print(answer.get(i));
+                        }
+                        System.out.println();
+                    }
+                    if (isFound == 5) {
+                        System.out.println("Bạn có muốn được gợi ý không? [Y/N]");
+                        char k = sc.next().charAt(0);
+                        if (k == 'Y' || k == 'y') {
+                            for (int i = 1; i < myWord.length()/2; i++) {
+                                answer.set(i + 1, myWord.charAt(i) + "");
+                            }
+                        }
+                        for (String c : answer) {
+                            System.out.print(c);
+                        }
+                        System.out.println();
+                    }
+                }
+                System.out.println();
+                System.out.println("Bạn có muốn sử dụng thêm chức nào của tôi nữa không?");
+                System.out.println("Nếu có thì nhập các chức năng mà bạn muốn sử dụng\n");
             }
             if (s == '8') {
-                System.out.println("Vui lòng nhập đường dẫn file bạn muốn import");
-            }
-            if (s == '9') {
-                System.out.println("Vui lòng nhập đường dẫn file bạn muốn export");
+                System.out.println("Nhập tên tệp bạn muốn nhập từ vựng từ:");
+                String fileName = sc.next();
+                //List<Word> words = read(fileName);
+//                for (Word word : words) {
+//                    WordManager.getWordManager().addWord(word);
+//                }
+                System.out.println("Đã nhập từ vựng từ tệp " + fileName);
+            } else if (s == '9') {
+                System.out.println("Nhập tên tệp bạn muốn xuất từ vựng ra:");
+                String fileName = sc.next();
+//                List<Word> words = WordManager.getWordManager().getWordList();
+//                writeWordsToFile(words, fileName);
+                System.out.println("Đã xuất từ vựng ra tệp " + fileName);
             }
         }
     }
