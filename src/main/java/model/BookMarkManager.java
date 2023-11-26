883bde2a2d3803dfa84cda7b4fe96c418cd0c3cd
hungsinh2k4 <monternsh@gmail.com>
2023-11-26 23:39:54 +0700

    update BookMarkManager

--- src/main/java/model/BookMarkManager.java
+++ src/main/java/model/BookMarkManager.java
@@ -4,11 +4,16 @@ import controller.LogInController;
 import database.UserDB;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class BookMarkManager extends Manager {
 
     private static BookMarkManager bookMarkManager;
     private final String BOOKMARK_PATH = "src/main/resources/data/wordBank.dat";
+
+    private final String PATH = "src/main/resources/data/export.dat";
+
+    private final String PATH1 = "src/main/resources/data/import.dat";
 
     public static BookMarkManager getInstance() throws IOException {
         if (bookMarkManager == null) {
@@ -21,6 +26,13 @@ public class BookMarkManager extends Manager {
         super();
         read(BOOKMARK_PATH);
         updateWordSpelling();
+    }
+
+    public static BookMarkManager getBookMarkManager() throws IOException {
+        if (bookMarkManager == null) {
+            bookMarkManager = new BookMarkManager();
+        }
+        return bookMarkManager;
     }
 
     @Override
@@ -43,6 +55,40 @@ public class BookMarkManager extends Manager {
         BookMarkManager.getInstance().getWordList().removeIf(word1 -> {
             return word1.equals(word);
         });
+    }
+
+
+    public void exportFile() throws IOException {
+        File file = new File(PATH);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        FileWriter fileWriter = new FileWriter(file);
+        for (Word word : wordList) {
+            fileWriter.write(word.getSpelling() + "\n");
+            //fileWriter.write("\n");
+            fileWriter.write(word.getContent() + "\n");
+        }
+        fileWriter.close();
+    }
+
+
+    public void importFile() throws IOException {
+        File file = new File(PATH1);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        Scanner scanner = new Scanner(file);
+        String s = "";
+        while (scanner.hasNextLine()) {
+            s += scanner.nextLine() + "\n";
+        }
+        String[] strings = s.split("\n");
+        for (int i = 0; i < strings.length; i += 2) {
+            Word word = new Word(strings[i], " ", strings[i + 1], " ");
+            wordList.add(word);
+        }
+        scanner.close();
     }
 
     public void saveDatabase() {
